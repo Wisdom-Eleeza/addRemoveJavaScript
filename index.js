4ae57e3e3939524ce90b506d13069e7fa88d67a7
Wisdom-Eleeza <wisdom.eleeza@amalitech.com>
2023-02-28 14:27:13 +0000

    Add, Edit and Remove JavaScript Functionality

--- index.js
+++ index.js
@@ -1,18 +1,73 @@
-let inputText = document.getElementById('txt')  
-
-function addLi(){
-    let listNode = document.getElementById('list')
-    let textNode = document.createTextNode(inputText.value)
-    liNode = document.createElement('li')
-
-    liNode.appendChild(textNode)
-    listNode.body.appendChild(liNode)
-}
-
-
-
-
-
+var inputText = document.getElementById("txt"),
+                 items = document.querySelectorAll("#list li"),
+                 tab = [], index;
+         
+             // get the selected li index using array
+             // populate array with li values
+             
+             for(var i = 0; i < items.length; i++){
+                 tab.push(items[i].innerHTML);
+             }
+             
+             // get li index onclick
+             for(var i = 0; i < items.length; i++){
+                 
+                 items[i].onclick = function(){
+                     index = tab.indexOf(this.innerHTML);
+                     console.log(this.innerHTML + " INDEX = " + index);
+                     // set the selected li value into input text
+                     inputText.value = this.innerHTML;
+                 };
+                 
+             }
+            
+            function refreshArray()
+            {
+                // clear array
+                tab.length = 0;
+                items = document.querySelectorAll("#list li");
+                // fill array
+                for(var i = 0; i < items.length; i++){
+                 tab.push(items[i].innerHTML);
+               }
+            }
+            function addLI(){
+                
+                var listNode = document.getElementById("list"),
+                    textNode = document.createTextNode(inputText.value),
+                    liNode = document.createElement("LI");
+                    
+                    liNode.appendChild(textNode);
+                    listNode.appendChild(liNode);
+                    
+                    refreshArray();
+                    
+                    // add event to the new LI
+                    
+                    liNode.onclick = function(){
+                     index = tab.indexOf(liNode.innerHTML);
+                     console.log(liNode.innerHTML + " INDEX = " + index);
+                     // set the selected li value into input text
+                     inputText.value = liNode.innerHTML;
+                 };
+                    
+             }
+             
+             function editLI(){
+                 // edit the selected li using input text
+                 items[index].innerHTML = inputText.value;
+                 refreshArray();
+              }
+              
+              function deleteLI(){
+                  
+                      refreshArray();
+                      if(items.length > 0){
+                          items[index].parentNode.removeChild(items[index]);
+                          inputText.value = "";
+                      }
+              }
+            
 /*
 To add and remove movies from a bookmark list, you can use the same approach as adding and removing bookmarks. 
 The main difference is that instead of storing the URL and title of the page, you will store the movie information such as its title, 
